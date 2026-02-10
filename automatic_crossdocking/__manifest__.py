2b559e5e52c2b4c0b53e41f52dac91f495b723a6
PrimateUY SAS <123416410+primateuy@users.noreply.github.com>
2026-02-10 13:10:49 -0300

    Update __manifest__.py

--- automatic_crossdocking/__manifest__.py
+++ automatic_crossdocking/__manifest__.py
@@ -17,7 +17,7 @@
     'depends': ['base', 'web','web_grid', 'purchase', 'purchase_stock', 'purchase_order_type', 'stock'],
 
     'data': [
-        'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
         'security/security.xml',
         'views/PurchaseOrderView.xml',
         'views/PurchaseOrderLineView.xml',
