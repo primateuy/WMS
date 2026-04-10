dbbddf82a9d8c3b90ee83b91e64aacd6ca78dfad
Alan Fránquez <santifranquez@gmail.com>
2026-04-10 18:44:54 -0300

    fix: mejora en el comportamiento para los movimientos y el estado de espera

--- integracion_wis/models/purchase_order.py
+++ integracion_wis/models/purchase_order.py
@@ -20,34 +20,34 @@ class PurchaseOrder(models.Model):
 
     enviadoWMS = fields.Boolean(string="Enviado de WMS", default=False)
 
-    def _create_picking(self):
-        res = super(PurchaseOrder, self)._create_picking()
+    # def _create_picking(self):
+    #     res = super(PurchaseOrder, self)._create_picking()
 
-        pickings = res if not isinstance(res, bool) else self.picking_ids
+    #     pickings = res if not isinstance(res, bool) else self.picking_ids
 
-        if pickings:
-            for picking in pickings:
-                if picking.move_ids:
+    #     if pickings:
+    #         for picking in pickings:
+    #             if picking.move_ids:
                     
-                    datosAPI = self.env['integracion_wis.integracion_wis'].search([], limit=1)
+    #                 datosAPI = self.env['integracion_wis.integracion_wis'].search([], limit=1)
 
-                    if datosAPI and datosAPI.apiLink and picking.state == picking.picking_type_id.estado_disparo_wis:
-                        response = datosAPI.insertarReferenciaRecepcion(picking)
+    #                 if datosAPI and datosAPI.apiLink and picking.state == picking.picking_type_id.estado_disparo_wis:
+    #                     response = datosAPI.insertarReferenciaRecepcion(picking)
                         
-                        if response and isinstance(response, dict):
-                            picking.with_context(skip_wms_integration=True).write({
-                                'idPedidoWMS': response.get('numeroInterfaz', ''),
-                                'codigo_unico': response.get('codigoUnico', ''),
-                                'wms_estado': 'enviado',
-                            })
-                            self.env['wms.integracion.log'].create({
-                                'fecha': fields.Datetime.now(),
-                                'nivel': 'info',
-                                'modelo': 'stock.picking',
-                                'texto': f"Integración exitosa con WIS",
-                                'picking_id': picking.id,
-                                'resultado': 'exito',
-                                'detalle': f"ID Pedido WMS: '{response.get('numeroInterfaz', '')}' | Código Único: '{response.get('codigoUnico', '')}'"
-                            })
+    #                     if response and isinstance(response, dict):
+    #                         picking.with_context(skip_wms_integration=True).write({
+    #                             'idPedidoWMS': response.get('numeroInterfaz', ''),
+    #                             'codigo_unico': response.get('codigoUnico', ''),
+    #                             'wms_estado': 'enviado',
+    #                         })
+    #                         self.env['wms.integracion.log'].create({
+    #                             'fecha': fields.Datetime.now(),
+    #                             'nivel': 'info',
+    #                             'modelo': 'stock.picking',
+    #                             'texto': f"Integración exitosa con WIS",
+    #                             'picking_id': picking.id,
+    #                             'resultado': 'exito',
+    #                             'detalle': f"ID Pedido WMS: '{response.get('numeroInterfaz', '')}' | Código Único: '{response.get('codigoUnico', '')}'"
+    #                         })
         
-        return res
+    #     return res
