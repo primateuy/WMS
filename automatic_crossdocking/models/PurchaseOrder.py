08870ba13495bf3c1c02e3f41352ddd72f0f5af7
dyturralbe <darylyturraldelopez@gmail.com>
2026-05-28 12:22:42 -0300

    [IMP][-] quitando validacion para testeo

--- automatic_crossdocking/models/PurchaseOrder.py
+++ automatic_crossdocking/models/PurchaseOrder.py
@@ -320,8 +320,8 @@ class PurchaseOrder(models.Model):
             if main_warehouse.id == warehouse.id:
                 esPrincipal = True;
 
-            if not warehouse.crossdocking_reception_type_id:
-                raise ValidationError(f"El almacén '{warehouse.name}' no tiene definido el tipo de operación para 'Recepción Crossdocking'.")
+            # if not warehouse.crossdocking_reception_type_id:
+            #     raise ValidationError(f"El almacén '{warehouse.name}' no tiene definido el tipo de operación para 'Recepción Crossdocking'.")
             
             if not esPrincipal:
                 if not main_warehouse.crossdocking_type_id or not main_warehouse.crossdocking_location_id or not main_warehouse.crossdocking_reception_type_id:
